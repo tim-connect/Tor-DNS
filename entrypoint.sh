1a62318100434b39d4a9c8c29cd52e74be215e7a
tim-connect <tim_connect@tutamail.com>
2025-06-13 14:37:10 +0200

    Update entrypoint.sh
    added colors and log prefixes to distinguish socat instances

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,11 @@ tor &
 sleep 20
 
 # Start socat to forward localhost:443 to the hidden resolver via Tor
-socat TCP4-LISTEN:443,bind=127.0.0.1,reuseaddr,fork SOCKS4A:127.0.0.1:dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion:443,socksport=9050 &
+socat TCP4-LISTEN:443,bind=127.0.0.1,reuseaddr,fork SOCKS5:127.0.0.1:dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion:443,socksport=9050 2> >(sed 's/^/\x1b[42m[onion resolver]\x1b[0m /') &
 sleep 2
 
 #backup public resolver
-socat TCP4-LISTEN:443,bind=127.0.0.2,reuseaddr,fork SOCKS4A:127.0.0.1:1.1.1.1:443,socksport=9050 &
+socat TCP4-LISTEN:443,bind=127.0.0.2,reuseaddr,fork SOCKS5:127.0.0.1:1.1.1.1:443,socksport=9050 2> >(sed 's/^/\x1b[45m[clearnet resolver]\x1b[0m /') &
 sleep 2
 
 cat << EOF >> /etc/hosts
