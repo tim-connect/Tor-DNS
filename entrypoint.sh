65632bc3a73ca17c52bc48c3afc0cc08683f5fa8
tim-connect <tim_connect@tutamail.com>
2025-06-14 14:57:52 +0200

    added second tor proxy
    Added a dedicated socks proxy for each resolver, increased fault tolerance

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,45 @@
 #!/bin/sh
-
 set -e
 
+# Add loopback alias
 ip addr add 127.0.0.2/32 dev lo
 
-tor &
-sleep 20
+# Start Tor for onion resolution
+tor --SocksPort 9050 --DataDirectory /tmp/tor-onion \
+  > >(sed 's/^/\x1b[42m[onion]\x1b[0m /') \
+  2> >(sed 's/^/\x1b[42m[onion ERR]\x1b[0m /' >&2) &
 
-# Start socat to forward localhost:443 to the hidden resolver via Tor
-socat TCP4-LISTEN:443,bind=127.0.0.1,reuseaddr,fork SOCKS5:127.0.0.1:dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion:443,socksport=9050 2> >(sed 's/^/\x1b[42m[onion resolver]\x1b[0m /') &
 sleep 2
 
-#backup public resolver
-socat TCP4-LISTEN:443,bind=127.0.0.2,reuseaddr,fork SOCKS5:127.0.0.1:1.1.1.1:443,socksport=9050 2> >(sed 's/^/\x1b[45m[clearnet resolver]\x1b[0m /') &
+# Start Tor for clearnet resolution
+tor --SocksPort 9051 --DataDirectory /tmp/tor-clearnet \
+  > >(sed 's/^/\x1b[41m[clearnet]\x1b[0m /') \
+  2> >(sed 's/^/\x1b[41m[clearnet ERR]\x1b[0m /' >&2) &
+
+sleep 45
+
+# Start socat for .onion DNS resolution
+socat TCP4-LISTEN:443,bind=127.0.0.1,reuseaddr,fork \
+  SOCKS5:127.0.0.1:dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion:443,socksport=9050 \
+  > >(sed 's/^/\x1b[44m[onion]\x1b[0m /') \
+  2> >(sed 's/^/\x1b[44m[onion ERR]\x1b[0m /' >&2) &
+
 sleep 2
 
+# Start backup resolver through clearnet Tor proxy
+socat TCP4-LISTEN:443,bind=127.0.0.2,reuseaddr,fork \
+  SOCKS5:127.0.0.1:1.1.1.1:443,socksport=9051 \
+  > >(sed 's/^/\x1b[45m[clearnet]\x1b[0m /') \
+  2> >(sed 's/^/\x1b[45m[clearnet ERR]\x1b[0m /' >&2) &
+
+sleep 2
+
+# Host overrides
 cat << EOF >> /etc/hosts
 127.0.0.1 dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion
 127.0.0.2 one.one.one.one
 EOF
 
-exec dnscrypt-proxy -config dnscrypt-proxy.toml
+sleep 2
+
+dnscrypt-proxy -config dnscrypt-proxy.toml
